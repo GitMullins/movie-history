082c2f9096ae89069237ed9547b74678ba4e862d
David Mullins <Mullins6323@gmail.com>
2019-06-02 23:41:53 -0500

    adding to watchlist. needs delete function via movieId

---
+++ src/javascripts/helpers/data/watchlistData.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const firebaseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getWatchlistById = uid => new Promise((resolve, reject) => {
+  axios.get(`${firebaseUrl}/watchlist.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((results) => {
+      const watchlistResults = results.data;
+      const watchlist = [];
+      Object.keys(watchlistResults).forEach((watchlistId) => {
+        watchlistResults[watchlistId].id = watchlistId;
+        watchlist.push(watchlistResults[watchlistId]);
+      });
+      resolve(watchlist);
+    })
+    .catch(err => reject(err));
+});
+
+const addToWatchlist = movie => axios.post(`${firebaseUrl}/watchlist.json`, movie);
+
+export default { addToWatchlist, getWatchlistById };
