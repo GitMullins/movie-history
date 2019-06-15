611a6abc153f5cf9f50354b7bae54d95f12c0abc
David Mullins <Mullins6323@gmail.com>
2019-06-15 11:47:16 -0500

    rating and star buttons are populating card

--- src/javascripts/components/movies/movies.js
+++ src/javascripts/components/movies/movies.js
@@ -42,9 +42,26 @@ const addToWatchlistEvent = (e) => {
       .then(() => getMovies(firebase.auth().currentUser.uid)) // eslint-disable-line no-use-before-define
       .catch(err => console.error('no update', err));
   }
-  // movieData.deleteMovie(movie)
-  //   .then(() => getMovies(firebase.auth().currentUser.uid)) // eslint-disable-line no-use-before-define
-  //   .catch(err => console.error('no deletion', err));
+};
+
+const deleteWatchlistMovieEvent = (e) => {
+  watchlistData.deleteMovie(e.target.id)
+    .then(() => getMovies(firebase.auth().currentUser.uid)) // eslint-disable-line no-use-before-define
+    .catch(err => console.error('delete watchlist err', err));
+};
+
+const rateMovieEvent = (e) => {
+  // console.error(e.target.id);
+  document.getElementById(e.target.id).classList.add('hide');
+  // console.error(document.getElementById(e.target.id));
+  const id = e.target.id.split('.')[1];
+  console.error(`stars.${id}`);
+  // document.getElementsByClassName(`stars.${id}`).classList.remove('hide');
+  document.getElementById(`rate1.${id}`).classList.remove('hide');
+  document.getElementById(`rate2.${id}`).classList.remove('hide');
+  document.getElementById(`rate3.${id}`).classList.remove('hide');
+  document.getElementById(`rate4.${id}`).classList.remove('hide');
+  document.getElementById(`rate5.${id}`).classList.remove('hide');
 };
 
 const addEvents = () => {
@@ -52,6 +69,14 @@ const addEvents = () => {
   const watchlistButtons = document.getElementsByClassName('watchlist');
   for (let i = 0; i < watchlistButtons.length; i += 1) {
     watchlistButtons[i].addEventListener('click', addToWatchlistEvent);
+  }
+  const deleteButtons = document.getElementsByClassName('delete');
+  for (let i = 0; i < deleteButtons.length; i += 1) {
+    deleteButtons[i].addEventListener('click', deleteWatchlistMovieEvent);
+  }
+  const editButtons = document.getElementsByClassName('rate');
+  for (let i = 0; i < editButtons.length; i += 1) {
+    editButtons[i].addEventListener('click', rateMovieEvent);
   }
 };
 
@@ -73,6 +98,13 @@ const domStringBuilder = (movies, watchlist) => {
     watchlistBuilder += '<h2>WATCHLIST</h2>';
     watchlistBuilder += `<h3 id="${movie.title}" class="title">${movie.title}</h3>`;
     watchlistBuilder += `<img class="img" id="${movie.imageUrl}" src="${movie.imageUrl}" alt="birthday location"/>`;
+    watchlistBuilder += `<button id="${movie.id}" class="btn btn-danger delete">DELETE</button>`;
+    watchlistBuilder += `<button id="rate.${movie.id}" class="btn btn-sm btn-warning rate">RATE</button>`;
+    watchlistBuilder += `<button id="rate1.${movie.id}" class="hide stars.${movie.id} btn btn-sm btn-warning">1 &#9733</button>`;
+    watchlistBuilder += `<button id="rate2.${movie.id}" class="hide stars.${movie.id} btn btn-sm btn-warning">2 &#9733</button>`;
+    watchlistBuilder += `<button id="rate3.${movie.id}" class="hide stars.${movie.id} btn btn-sm btn-warning">3 &#9733</button>`;
+    watchlistBuilder += `<button id="rate4.${movie.id}" class="hide stars.${movie.id} btn btn-sm btn-warning">4 &#9733</button>`;
+    watchlistBuilder += `<button id="rate5.${movie.id}" class="hide stars.${movie.id} btn btn-sm btn-warning">5 &#9733</button>`;
     watchlistBuilder += '</div>';
   });
   watchlistBuilder += '</div>';
